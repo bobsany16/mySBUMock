56d69d04545d94f0a87d6640c075c464398174cb
Linh Nguyen <linh.ng754@gmail.com>
2020-05-25 21:23:03 -0400

    Initial Styling via module.css

---
+++ mySBU/src/components/Colors.js
@@ -0,0 +1,16 @@
+export const Colors ={
+    'White': '#ffffff',
+    'Brown': '#471c14',
+    'BrownLight': '#893527',
+    'DarkBrown': '#32140e',
+    'BrownDarkest': '#240e0a',
+    'BrownLightest': '#f9eeeb',
+    'Orange': '#c8503b',
+    'Yellow': '#fdb725',
+    'DarkYellow': '#ad7702',
+    'DarkestYellow': '#906301',
+    'LightYellow': '#fee3a8',
+    'LightestYellow': 'fff1d4',
+    'YellowWhite': '#fffbf2',
+
+}
--- mySBU/src/components/NavBar.js
+++ mySBU/src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "../styles/navBarStyles.module.css";
 
 const NavBar = () => {
     return (
-        <div className="myNavBar">
+        <div className={styles.myNavBar}>
             <h1>NavBar component</h1>
         </div>
     )
