5a76099a9494811445a387a13b22a81861bf7694
Linh Nguyen <linh.ng754@gmail.com>
2020-05-27 19:11:04 -0400

    Added Hamburger Menu

--- mySBU/src/components/NavBar.js
+++ mySBU/src/components/NavBar.js
@@ -1,13 +1,71 @@
-import React from "react";
-import styles from "../styles/navBarStyles.module.css";
+import React from "react"
+import styles from "../styles/navBarStyles.module.css"
+import { Link } from "gatsby"
+import HamburgerMenu from "react-hamburger-menu"
 
-const NavBar = () => {
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+    // this.isOpen = this.props.value;
+    // this.menuClicked=this.props.value;
+    this.state = {
+      open: false,
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      open: !this.state.open,
+    })
+  }
+
+  render() {
     return (
-        <div className={styles.myNavBar}>
-            <h1>NavBar component</h1>
-        </div>
+      <nav className={styles.myNavBar}>
+        <span className={styles.navBarToggle}>
+          <HamburgerMenu
+            isOpen={this.state.open}
+            menuClicked={this.handleClick.bind(this)}
+            color={"#ffffff"}
+            width={20}
+            height={17}
+          ></HamburgerMenu>
+        </span>
+
+        <Link to="/" className={styles.logo}>
+          My.SBU
+        </Link>
+
+        <ul className={styles.mainNav} id="js-menu">
+          <li>
+            <Link to="/" className={styles.navLinks}>
+              Office 365 Mail
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className={styles.navLinks}>
+              Moodle
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className={styles.navLinks}>
+              Calendar
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className={styles.navLinks}>
+              Directory
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className={styles.navLinks}>
+              Tech Assistance
+            </Link>
+          </li>
+        </ul>
+      </nav>
     )
+  }
 }
 
-
-export default NavBar;
+export default NavBar
